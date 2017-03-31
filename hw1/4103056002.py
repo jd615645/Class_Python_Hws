2eaafbce4384c41fe5d63266df3c922d524652c3
jd615645 <jd615645@gmail.com>
2017-03-31 20:01:28 +0800

    :cat2:初步完成

--- hw1/4103056002.py
+++ hw1/4103056002.py
@@ -2,12 +2,15 @@
 from __future__ import print_function
 from colorclass import Color, Windows
 from terminaltables import SingleTable
-import os, random
+import os, random, re
+import curses
 
-maxItem = 10
+maxItem = 0
 gameBoard = []
 
 def getItem(num):
+  if (num == ''):
+    return ''
   item = [
     Color('{autoblack}A{/black}'),
     Color('{autored}B{/red}'),
@@ -16,9 +19,9 @@ def getItem(num):
     Color('{autoblue}E{/blue}'),
     Color('{automagenta}F{/magenta}'),
     Color('{autocyan}G{/cyan}'),
-    Color('{autowhite}H{/white}'),
+    Color('{autobgyellow}{autoblue}H{/blue}{/bgyellow}'),
     Color('{autobgred}{autoblack}I{/black}{/bgred}'),
-    Color('{autobgred}{autored}J{/red}{/bgred}'),
+    Color('{autobgblue}{autowhite}J{/red}{/bgred}'),
     Color('{autobgred}{autogreen}K{/green}{/bgred}'),
     Color('{autobgred}{autoyellow}L{/yellow}{/bgred}'),
     Color('{autobgred}{autoblue}M{/blue}{/bgred}'),
@@ -27,7 +30,7 @@ def getItem(num):
     Color('{autobgred}{autowhite}P{/white}{/bgred}'),
     Color('{autobggreen}{autoblack}Q{/black}{/bggreen}'),
     Color('{autobggreen}{autored}R{/red}{/bggreen}'),
-    Color('{autobggreen}{autogreen}S{/green}{/bggreen}'),
+    Color('{autobgblue}{autoyellow}S{/green}{/bggreen}'),
     Color('{autobggreen}{autoyellow}T{/yellow}{/bggreen}'),
     Color('{autobggreen}{autoblue}U{/blue}{/bggreen}'),
     Color('{autobggreen}{automagenta}V{/magenta}{/bggreen}'),
@@ -36,11 +39,7 @@ def getItem(num):
     Color('{autobgyellow}{autoblack}Y{/black}{/bgyellow}'),
     Color('{autobgyellow}{autored}Z{/red}{/bgyellow}')
   ]
-  print(num)
   return item[int(num)]
-
-def tableNum(num):
-  return get_color_string(bcolors.WHITE, num)
 
 def initTable():
   for row in range(10):
@@ -58,15 +57,53 @@ def initTable():
     
 def checkLine(row, col):
   # right
-  print('right: ', gameBoard[row][col], gameBoard[row][col-1], gameBoard[row][col-2], sep=',')
-  print('down: ', gameBoard[row][col], gameBoard[row-1][col], gameBoard[row-2][col], sep=',')
-  if (gameBoard[row][col] == gameBoard[row][col-1] == gameBoard[row][col-2]):
-    return False
+  if(gameBoard[row][col-1] != ''):
+    if (gameBoard[row][col] == gameBoard[row][col-1] == gameBoard[row][col-2]):
+      return True
   # down
-  if (gameBoard[row][col] == gameBoard[row-1][col] == gameBoard[row-2][col]):
-    return False
+  if(gameBoard[row-1][col] != ''):
+    if (gameBoard[row][col] == gameBoard[row-1][col] == gameBoard[row-2][col]):
+      return True
+  return False
+
+def checkHaveLine():
+  needremove = []
+  # find col
+  for row in range(10):
+    repect = 0
+    keyItem = gameBoard[row][0]
+    for col in range(1, 10):
+      if (gameBoard[row][col] == keyItem):
+        repect += 1
+        if (repect == 2):
+          for i in range(3):
+            needremove.append([row, col-i])
+        elif (repect > 2):
+          needremove.append([row, col])
+      else:
+        repect = 0
+        keyItem = gameBoard[row][col]
+  # find row
+  for col in range(10):
+    repect = 0
+    keyItem = gameBoard[0][col]
+    for row in range(1, 10):
+      if (gameBoard[row][col] == keyItem):
+        repect += 1
+        if (repect == 2):
+          for i in range(3):
+            needremove.append([row-i, col])
+        elif (repect > 2):
+          needremove.append([row, col])
+      else:
+        repect = 0
+        keyItem = gameBoard[row][col]
+    
+  if (len(needremove) != 0):
+    return needremove
   else:
-    return True
+    return False
+
 
 def drawTable():
   uiTable = []
@@ -83,7 +120,6 @@ def drawTable():
         if (col == 0 or col == 10):
           ary.append(row)
         else:
-          print(gameBoard[row-1][col-1])
           ary.append(getItem(gameBoard[row-1][col-1]))
     uiTable.append(ary)
 
@@ -92,11 +128,144 @@ def drawTable():
   showTable.inner_row_border = True
   print(showTable.table)
 
+def inputCoordinate():
+  keyin = raw_input('請輸入座標(ex: 4,5): ')
+  check = re.search('^\d,\d$', keyin)
+
+  if (check):
+    coordinate = check.group().split(',')
+    row = int(coordinate[0])
+    col = int(coordinate[1])
+    if (row<1 or row>maxItem-1 or col<1 or col>maxItem-1 ):
+      return False
+    else:
+      return [row, col]
+  else:
+    return False
+    
+def inputmove(coordinate):
+  keyin = raw_input('請輸入移動方向(ex: w or a or s or d): ')
+  check = re.search('^[wasd]$', keyin)
+
+  row = coordinate[0]
+  col = coordinate[1]
+  if(check):
+    if (keyin == 'w'):
+      if (coordinate[0] > 1):
+        return keyin
+    elif (keyin == 'a'):
+      if (coordinate[1] > 1):
+        return keyin
+    elif (keyin == 's'):
+      if (coordinate[0] < 9):
+        return keyin
+    elif (keyin == 'd'):
+      if (coordinate[1] < 9):
+        return keyin
+    return False
+  else:
+    return False
+
+def moveCoordinate(coordinate, move):
+  row = coordinate[0]
+  col = coordinate[1]
+  if (move == 'w'):
+    return [row-1, col]
+  elif (move == 'a'):
+    return [row, col-1]
+  elif (move == 's'):
+    return [row+1, col]
+  elif (move == 'd'):
+    return [row, col+1]
+
+def errorKeyin():
+  msg = [u'請照格式輸入']
+  showTable = SingleTable(msg)
+  print(showTable.table)
+
+def removeItem(needRemove):
+  point = 0
+  for i in needRemove:
+    row = i[0]
+    col = i[1]
+    if (gameBoard[row][col] != ''):
+      gameBoard[row][col] = ''
+      point += 1
+  return point
+
+def showPoint(points, rounds):
+  show = [
+    ['Points', points],
+    ['Last Round', rounds]
+  ]
+  showTable = SingleTable(show)
+  print(showTable.table)
+
+def dropDown():
+  for col in range(10):
+    for row in range(9, -1, -1):
+      while True:
+        if (gameBoard[row][col] == ''):
+          for dropRow in range(row, 0, -1):
+            gameBoard[dropRow][col] = gameBoard[dropRow-1][col]
+          gameBoard[0][col] = random.randint(0, maxItem-1)
+        else:
+          break
+
+def swap(coordinate, moveToCoordinate):
+  row_1 = coordinate[0]-1
+  col_1 = coordinate[1]-1
+  row_2 = moveToCoordinate[0]-1
+  col_2 = moveToCoordinate[1]-1
+
+  gameBoard[row_1][col_1], gameBoard[row_2][col_2] = gameBoard[row_2][col_2], gameBoard[row_1][col_1]
+
 def main():
-  global maxItem
-  maxItem = input('想要幾種顏色(1-26)？')
+  global maxItem 
+  maxItem = input('想要幾種顏色(3-26)？')
+  rounds = 20
+  points = 0
   initTable()
-  drawTable()
+
+  haveError = False
+
+
+
+  while (rounds>0):
+    drawTable()
+    showPoint(points, rounds)
+    if (haveError):
+      haveError = False
+      errorKeyin()
+    
+    coordinate = inputCoordinate()
+    if (not coordinate):
+      haveError = True
+      continue
+
+    move = inputmove(coordinate)
+    if (not move):
+      haveError = True
+      continue
+    
+    moveTo = moveCoordinate(coordinate, move)
+
+    swap(coordinate, moveTo)
+    drawTable()
+    haveLine = checkHaveLine()
+
+    if (haveLine):
+      points += removeItem(haveLine)
+      dropDown()
+      check = checkHaveLine()
+      while (check):
+        points += removeItem(check)
+        dropDown()
+        check = checkHaveLine()
+      rounds -= 1
+    else:
+      swap(coordinate, moveTo)
+
 
 if __name__ == '__main__':
   main()
--- hw1/test.py
+++
@@ -1,7 +0,0 @@
-# -*- coding: utf-8 -*-
-from __future__ import print_function
-
-from colorclass import Color
-color_string = Color('{red}Test{/red}')
-
-print(color_string)
