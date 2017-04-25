dbcd4a5308376f89b9843c45de6d54dc9b1bda9d
jd615645 <jd615645@gmail.com>
2017-04-25 19:00:08 +0800

    :cat2:20170425

--- hw2/client.py
+++ hw2/client.py
@@ -1,26 +1,110 @@
 # -*- coding: utf-8 -*-
 from __future__ import print_function
-import socket
+import select, random, json, os, socket
+from Tkinter import *
 
-def connectServer():
-  HOST = '127.0.0.1'
-  PORT = 8001
+# 0 => join game
+# 1 => wait start
+# 2 => wait playing
+# 3 => playing
+# 4 => wait ending
+nowStatus = 0
+playerID = -1
 
-  s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-  s.connect((HOST, PORT))
-  while True:
-    cmd = raw_input("Please input msg:")
-    s.send(cmd)
-    data = s.recv(1024)
-    print(data)
-
-      #s.close()
-
-def init():
-  print('hi')
+getMsg = False
 
 def main():
-  init()
+  global nowStatus, getMsg
 
+  host = '127.0.0.1'
+  port = 5000
+
+  s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+  s.settimeout(2)
+
+  # connect to remote host
+  try :
+    s.connect((host, port))
+  except :
+    print('Unable to connect')
+    sys.exit()
+
+  print('Connected to remote host. Start sending messages')
+
+  while 1:
+    socket_list = [sys.stdin, s]
+
+    # Get the list sockets which are readable
+    read_sockets, write_sockets, error_sockets = select.select(socket_list , [], [])
+
+    for sock in read_sockets:
+      #incoming message from remote server
+
+      if sock == s:
+        # 接收資料
+        data = sock.recv(4096)
+        if not data :
+          print('Disconnected from chat server')
+          sys.exit()
+        else :
+          if (getMsg):
+            print(data)
+            getMsg = False
+          
+          if (nowStatus == 0):
+            playerID = int(data)
+            nowStatus = 1
+            print('player: ' + data)
+            print('按任何按鍵準備遊戲')
+          elif (nowStatus == 1 and data == 'start'):
+            print('開始遊戲')
+            if (playerID == 1):
+              print('輪到你了')
+              print('是否繼續要牌')
+              nowStatus = 3
+              getMsg = True
+              s.send('sendData')
+            else:
+              print('輪到 player1')
+              nowStatus = 2
+          elif (nowStatus == 2 and data == str(playerID)):
+            print('輪到你了')
+            print('是否繼續要牌')
+            nowStatus = 3
+          elif (nowStatus == 3 and data == 'stand'):
+            nowStatus = 4
+
+          print('-----')
+          print('send: ' + data)
+          print('nowStatus: ' + str(nowStatus))
+          print('-----')
+          
+      #user entered a message
+      else :
+        # 發送資料
+        if (nowStatus == 1):
+          msg = raw_input()
+          print('等待其他玩家')
+          s.send('ready')
+        elif (nowStatus == 2):
+          msg = raw_input()
+          print('還未輪到你')
+        elif (nowStatus == 3):
+          msg = raw_input()
+          if (msg == 'y'):
+            print('hit')
+            s.send('hit')
+            getMsg = True
+          else:
+            if (playerID != 0):
+              s.send('stand')
+            else:
+              s.send('endGame')
+            nowStatus = 4
+            print('等待遊戲結束')
+        elif (nowStatus == 4):
+          print('等待遊戲結束')
+
+#main function
 if __name__ == '__main__':
   main()
--- hw2/server.py
+++ hw2/server.py
@@ -1,72 +1,71 @@
 # -*- coding: utf-8 -*-
 from __future__ import print_function
-import socket
-import random
-import Tkinter as tk
+import select, random, json, os, socket
 
 poker = []
 pokerUsed = 0
+playerLimit = 4
 
-havePoker = 0
-banker = []
+def broadcast_data (sock, message):
+  for socket in CONNECTION_LIST:
+    if socket != server_socket :
+      try :
+        socket.send(message)
+      except :
+        socket.close()
+        CONNECTION_LIST.remove(socket)
 
-def windowInit():
-  blackjack = tk.Tk()
-  blackjack.title('blackjack')
-  blackjack.geometry('400x300')
+class Player:
+  def __init__(self, cards, points, safe):
+    self.cards = cards
+    self.points = points
+    self.safe = safe
 
-  hit = tk.Button(blackjack, text='hit')
-  stand = tk.Button(blackjack, text='stand')
+  def __new___(cls, cards, points, safe):
+    return object.__new__(cls)
 
-  hit.grid(column=0, row=0)
-  stand.grid(column=1, row=0)
-  blackjack.mainloop()
+  def hit(self):
+    global pokerUsed
+    if (self.safe):
+      self.cards.append(poker[pokerUsed])
 
-def serverOn():
-  HOST = '127.0.0.1'
-  PORT = 8001
+      getPoker = poker[pokerUsed]
+      pokerUsed += 1
 
-  s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-  s.bind((HOST, PORT))
-  s.listen(5)
+      self.calcPoint()
 
-  print('Server start at: %s:%s' %(HOST, PORT))
-  print('wait for connection...')
+      if (self.points > 21):
+        self.safe = False
 
-  while True:
-    conn, addr = s.accept()
-    print('Connected by ', addr)
+  def stand(self):
+    self.safe = False
 
-  while True:
-    data = conn.recv(1024)
-    print(data)
+  def calcPoint(self):
+    totNum = 0
 
-    conn.send("server received you message.")
+    for num in self.cards:
+      pokerNum = num % 13
+      if (pokerNum == 0):
+        pokerNum = 13
+      if (pokerNum >= 10):
+        totNum += 10
+      elif (pokerNum == 1):
+        if (totNum+11 > 21):
+          totNum += 1
+        else:
+          totNum += 11
+      else:
+        totNum += pokerNum
+    self.points = totNum
 
-  conn.close()
+  def getData(self):
+    return {
+      'cards': self.cards,
+      'point': self.points
+    }
 
-def checkPoker(num):
-  pokerNum = num % 13
-  if (pokerNum == 0):
-    pokerNum = 13
-
-  pokerType = ''
-  pokerTypeCalc = num / 13
-  if (pokerTypeCalc == 0):
-    # ♠
-    pokerType = 's'
-  elif (pokerTypeCalc == 1):
-    # ♥
-    pokerType = 'h'
-  elif (pokerTypeCalc == 2):
-    # ♦
-    pokerType = 'd'
-  elif (pokerTypeCalc >= 3):
-    # ♣
-    pokerType = 'c'
-  return {'type': pokerType, 'num': pokerNum}
-
-def init():
+def shuffle():
+  pokerUsed = 0
   for i in range(1, 53):
     poker.append(i)
 
@@ -74,20 +73,124 @@ def init():
   for i in range(52):
     pokerRand = random.randint(0, 51)
     poker[i], poker[pokerRand] = poker[pokerRand], poker[i]
-    # print(checkPoker(poker[i]))
-
-def needPoker():
-  global pokerUsed
-  banker.append(poker[pokerUsed])
-  pokerUsed += 1
-def getStart():
-
-  for i in range(2):
-      needPoker()
-
-def main():
-  init()
-  windowInit()
 
 if __name__ == '__main__':
-  main()
+
+  # List to keep track of socket descriptors
+  CONNECTION_LIST = []
+  RECV_BUFFER = 4096
+  # Advisable to keep it as an exponent of 2
+  PORT = 5000
+
+  server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+  # this has no effect, why ?
+  server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+  server_socket.bind(('0.0.0.0', PORT))
+  server_socket.listen(10)
+
+  # Add server socket to the list of readable connections
+  CONNECTION_LIST.append(server_socket)
+
+
+  print('Chat server started on port ' + str(PORT))
+  players = []
+  ready = 0
+  whoPlay = 1
+  start = False
+  while 1:
+    # Get the list sockets which are ready to be read through select
+    read_sockets,write_sockets,error_sockets = select.select(CONNECTION_LIST,[],[])
+
+    for sock in read_sockets:
+      #New connection
+      if sock == server_socket:
+        # Handle the case in which there is a new connection recieved through server_socket
+        sockfd, addr = server_socket.accept()
+        CONNECTION_LIST.append(sockfd)
+
+        print('Client (%s, %s) connected' % addr)
+        if (len(players) <= 5):
+          sockfd.send(str(len(players)))
+          players.append(Player([], 0, True))
+
+        # broadcast_data(sockfd, '[%s:%s] entered roomn\n' % addr)
+
+      #Some incoming message from a client
+      else:
+        # Data recieved from client, process it
+        try:
+          # In Windows, sometimes when a TCP program closes abruptly,
+          # a 'Connection reset by peer' exception will be thrown
+          data = sock.recv(RECV_BUFFER)
+
+          if data:
+            print('data: ' + data)
+
+            if (data == 'ready'):
+              ready += 1
+
+            if (len(players) == ready and ready > 1 and (not start)):
+              shuffle()
+              broadcast_data(sock, 'start')
+              for player in players:
+                for i in range(2):
+                  player.hit()
+              start = True
+              print('whoPlay: ' + str(whoPlay))
+
+            if (data == 'hit'):
+              if (players[whoPlay].safe):
+                print('hit')
+                players[whoPlay].hit()
+                data = 'sendData'
+              else:
+                print('stand')
+                data = 'stand'
+            
+            if (data == 'stand'):
+              if (whoPlay ==0):
+                # use endGame fun
+                data = 'endGame'
+              else:
+                whoPlay += 1
+                if (whoPlay > len(players)-1):
+                  whoPlay = 0
+                broadcast_data(sock, str(whoPlay))
+                data = 'sendData'
+            
+            if (data == 'sendData'):
+              print('sendData')
+              sendData = []
+              for player in players:
+                sendData.append(player.getData())
+              print(str(sendData))
+              broadcast_data(sock, str(sendData))
+
+            if (data == 'endGame'):
+              print('---遊戲結果---')
+              pointList = []
+              for player in players:
+                if (player.points > 21):
+                  pointList.append(0)
+                else:
+                  pointList.append(player.points)
+              bankerPoint = pointList[0]
+              playerNum = 1
+              for point in range(1, len(pointList)):
+                if (bankerPoint >= point):
+                  print('Player' + playerNum + 'vs 莊家 莊家勝利')
+                else:
+                  print('Player' + playerNum + 'vs 莊家 Player' + playerNum + '勝利')
+                playerNum += 1
+              print('--------------')
+                
+
+        except:
+          broadcast_data(sock, 'Client (%s, %s) is offline' % addr)
+          print('Client (%s, %s) is offline' % addr)
+          whoPlay -= 1
+          sock.close()
+          CONNECTION_LIST.remove(sock)
+          continue
+
+  server_socket.close()
